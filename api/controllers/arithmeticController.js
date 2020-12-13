73e7e9dce17d7dea2fce2d3ad1b50650f2a0f542
eccchange <75925504+eccchange@users.noreply.github.com>
2020-12-13 15:21:18 +0800

    Add comment for addition

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //+operator is necessary for coerce variable to number to aviod string concatenation.
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
